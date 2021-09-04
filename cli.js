e7cd0ff655073fe4542a8ab3bbc7a03c62bc7900
mk <mk@mks-MacBook-Air.local>
2021-09-04 13:01:57 -0400

    Updates:

---
+++ cli.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+// bring in all the tools needed for text display
+var figlet = require('figlet');
+var clear = require('clear');
+var chalk = require('chalk');
+
+// store any arguments passed in using yargs
+var yargs = require('yargs').argv;
+
+console.log("launching it.")
