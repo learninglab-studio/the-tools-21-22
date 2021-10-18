71780b58e8d731fb56451681a7ba6336e5ef51d3
mk <mk@mks-macbook-air.lan>
2021-10-17 23:17:49 -0400

    Updates:

--- cli.js
+++ cli.js
@@ -6,6 +6,8 @@ var clear = require('clear');
 var chalk = require('chalk');
 const mk = require('./tools/mk/index')
 const rename = require('./tools/ingest/rename')
+const makeFolders = require('./tools/ingest/make-folders')
+
 
 // store any arguments passed in using yargs
 var yargs = require('yargs').argv;
@@ -16,4 +18,6 @@ if (yargs.mk) {
     mk(yargs)
 } else if (yargs.rename) {
     rename(yargs.rename)
+} else if (yargs.makefolders) {
+    makeFolders(yargs.makefolders)
 }
---
+++ tools/ingest/make-folders.js
@@ -0,0 +1,43 @@
+const chalk = require("chalk")
+const fs = require("fs")
+const path = require("path")
+
+const standardFolders = [
+    `amx.a.a1`,
+    `amx.a.a2`,
+    `amx.a.a3`,
+    `amx.a.a4`,
+    `amx.a.a5`,
+    `amx.a.a6`,
+    `amx.a.a7`,
+    `amx.a.a8`,
+    `amx.a.v1`,
+    `amx.a.v2`,
+    `amx.a.v3`,
+    `amx.a.v4`,
+    `amx.a.v5`,
+    `amx.a.v6`,
+    `amx.a.v7`,
+    `amx.a.v8`,
+    `amx.a.drp`,
+    `amx.a.pgm`,
+    `c300.a.qt`,
+    `c300.b.qt`,
+]
+
+const makeFolders = async function(folderPath) {
+    console.log(chalk.blue(`*************\nlaunching makeFolder with folderpath:\n${JSON.stringify(folderPath, null, 4)}`))
+    // TODO: validate shoot path
+    // TODO: handle --amx value for different amx devices
+    for (let index = 0; index < standardFolders.length; index++) {
+        const element = standardFolders[index];
+        fs.mkdirSync(path.join(folderPath, element))
+    }
+}
+
+async function changeTheNames(operationArray) {
+    console.log(`renaming x to y`);
+    return("array of naming operations");
+}
+
+module.exports = makeFolders
